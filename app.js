1c76255f24d85bc064cfa450d0ea75dfc8789cda
Varsha <varshagangadhar04@gmail.com>
2025-11-20 15:58:45 +0530

    added web servers

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ console.log('Hello from Docker & GitHub\!');
 setInterval(() => {
   console.log("Hello from Docker & GitHub!");
 }, 2000);
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello Buddy! Your Node app is running in Docker!");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000...");
+});
+
